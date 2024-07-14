1f46b450ad287e9c8ab206fd582f2527c6abbcaf
Barasa <timbaros87@gmail.com>
2024-07-14 23:28:35 +0300

    Inser details

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE bears (
 id INTEGER PRIMARY KEY,
 name TEXT,
+age TEXT,
 sex TEXT,
 color TEXT,
 temperament TEXT,
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Playful", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Black", "Joyful", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Brown", "Playful", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Brown", "Fearful", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "Blond", "Joyful", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "M", "Black", "Silent", "False");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, "F", "Brown", "Joyful", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "F", "Brown", "sILENT", "True");
